776f392a9a4393b7a516f1a99f817225d2fe7df7
PrabhaniR <prabhani.rajakaruna@gmail.com>
2018-11-06 15:40:11 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,8 @@
+def do_something(something):
+    """
+    Prints the argument passed to do_something
+    """
+    print(something)# this will make it much easier in future problems to see that something is actually happening
 
 def power(x, y):
     z = x**y
